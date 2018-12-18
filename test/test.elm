bb6c197681d38713fb767139148debe4132bc05c
takashi tokumoto <jazzm@ster.jp.net>
2018-12-19 00:46:29 +0900

    これがワイのコミットや！

--- test/test.elm
+++ test/test.elm
@@ -7,7 +7,7 @@ view model =
                         span [] [ text "link" ] ] ],
                 li [] [
                     a [ href "aaa.html" ] [
-                        span [] [ text "link" ] ] ],
+                        span [] [ text "link link link" ] ] ],
                 li [] [
                     a [ href "aaa.html" ] [
                         span [] [ text "link" ] ] ] ] ],
@@ -15,19 +15,4 @@ view model =
             tbody [] [
                 tr [] [
                     td [ colspan 3, rowspan 3 ] [ text "td" ] ] ] ] ]
-    div [ class "wrapper", id "wrapper" ] [
-        nav [ class "gNav", id "gNav" ] [
-            ul [ class "gNav__list" ] [
-                li [] [
-                    a [ href "aaa.html" ] [
-                        span [] [ text "link" ] ] ],
-                li [] [
-                    a [ href "aaa.html" ] [
-                        span [] [ text "link" ] ] ],
-                li [] [
-                    a [ href "aaa.html" ] [
-                        span [] [ text "link" ] ] ] ] ],
-        table [ class "table_class" ] [
-            tbody [] [
-                tr [] [
-                    td [ colspan 3, rowspan 3 ] [ text "td" ] ] ] ] ]
+    
