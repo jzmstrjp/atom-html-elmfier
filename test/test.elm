20a990c793fed99ce6d8d57a04d9a239b6a6e64a
takashi tokumoto <jazzm@ster.jp.net>
2018-12-19 00:07:48 +0900

    定期

---
+++ test/test.elm
@@ -0,0 +1,33 @@
+view model =
+    div [ class "wrapper", id "wrapper" ] [
+        nav [ class "gNav", id "gNav" ] [
+            ul [ class "gNav__list" ] [
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ],
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ],
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ] ] ],
+        table [ class "table_class" ] [
+            tbody [] [
+                tr [] [
+                    td [ colspan 3, rowspan 3 ] [ text "td" ] ] ] ] ]
+    div [ class "wrapper", id "wrapper" ] [
+        nav [ class "gNav", id "gNav" ] [
+            ul [ class "gNav__list" ] [
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ],
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ],
+                li [] [
+                    a [ href "aaa.html" ] [
+                        span [] [ text "link" ] ] ] ] ],
+        table [ class "table_class" ] [
+            tbody [] [
+                tr [] [
+                    td [ colspan 3, rowspan 3 ] [ text "td" ] ] ] ] ]
