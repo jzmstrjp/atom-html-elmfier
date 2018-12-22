28690a3dbf8011b7f115f2751d093d6760534342
ikngtty <ikngtty@gmail.com>
2018-12-23 00:38:56 +0900

    style: resolve linter errors.

--- spec/helpers/helper.js
+++ spec/helpers/helper.js
@@ -4,9 +4,9 @@ export default {
   handleFileInEditor: async (filepath, callback) => {
     const editor = await atom.workspace.open(filepath);
     const value = callback(editor);
-    const closed =
-      await atom.workspace.paneForItem(editor).destroyItem(editor, true);
+    const closed = await atom.workspace.paneForItem(editor)
+      .destroyItem(editor, true);
     expect(closed).toBe(true);
     return value;
-  }
+  },
 };
--- spec/html-elmfier-spec.js
+++ spec/html-elmfier-spec.js
@@ -1,8 +1,8 @@
 'use babel';
 
-import path from 'path'
-import HtmlElmfier from '../lib/html-elmfier';
-import helper from './helpers/helper'
+import path from 'path';
+// import HtmlElmfier from '../lib/html-elmfier';
+import helper from './helpers/helper';
 
 // Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
 //
@@ -10,15 +10,16 @@ import helper from './helpers/helper'
 // or `fdescribe`). Remove the `f` to unfocus the block.
 
 describe('HtmlElmfier', () => {
-  let workspaceElement, activationPromise;
-  const pathCopyFrom =
-    path.resolve(__dirname, './fixtures/copyFrom.html');
-  const pathCopyTo =
-    path.resolve(__dirname, './fixtures/copyTo.elm');
-  const pathExpected =
-    path.resolve(__dirname, './fixtures/expected.elm');
-  const pathExpectedIfNoExec =
-    path.resolve(__dirname, './fixtures/expectedIfNoExec.elm');
+  let workspaceElement;
+  let activationPromise;
+  const pathCopyFrom = path.resolve(__dirname,
+    './fixtures/copyFrom.html');
+  const pathCopyTo = path.resolve(__dirname,
+    './fixtures/copyTo.elm');
+  const pathExpected = path.resolve(__dirname,
+    './fixtures/expected.elm');
+  const pathExpectedIfNoExec = path.resolve(__dirname,
+    './fixtures/expectedIfNoExec.elm');
 
   beforeEach(() => {
     workspaceElement = atom.views.getView(atom.workspace);
@@ -26,64 +27,55 @@ describe('HtmlElmfier', () => {
   });
 
   it('converts html to elm.', () => {
-    const expectedPromise =
-      helper.handleFileInEditor(
-        pathExpected,
-        editor => editor.getText()
-      );
+    const expectedPromise = helper.handleFileInEditor(
+      pathExpected,
+      editor => editor.getText(),
+    );
 
-    const actualPromise = helper.handleFileInEditor(pathCopyFrom, editor => {
+    const actualPromise = helper.handleFileInEditor(pathCopyFrom, (editor) => {
       editor.setSelectedBufferRange([[11, 4], [28, 10]]);
       // editor.copySelectedText();
       return editor.getSelectedText();
-    }).then(copiedText =>
-      helper.handleFileInEditor(pathCopyTo, editor => {
+    }).then(copiedText => helper.handleFileInEditor(pathCopyTo, (editor) => {
+      editor.setCursorBufferPosition([1, 4]);
+      // editor.pasteText();
+      editor.insertText(copiedText);
+      return editor.getText();
+    }));
+
+    waitsForPromise(() => Promise.all([
+      activationPromise,
+      expectedPromise,
+      actualPromise,
+    ]).then((values) => {
+      const expected = values[1];
+      const actual = values[2];
+      expect(actual).toBe(expected);
+    }));
+  });
+
+  it('can be toggled.', () => {
+    const expectedPromiseIfOn = helper.handleFileInEditor(
+      pathExpected,
+      editor => editor.getText(),
+    );
+    const expectedPromiseIfOff = helper.handleFileInEditor(
+      pathExpectedIfNoExec,
+      editor => editor.getText(),
+    );
+
+    const createActualPromise = () => {
+      atom.commands.dispatch(workspaceElement, 'html-elmfier:toggle');
+      return helper.handleFileInEditor(pathCopyFrom, (editor) => {
+        editor.setSelectedBufferRange([[11, 4], [28, 10]]);
+        // editor.copySelectedText();
+        return editor.getSelectedText();
+      }).then(copiedText => helper.handleFileInEditor(pathCopyTo, (editor) => {
         editor.setCursorBufferPosition([1, 4]);
         // editor.pasteText();
         editor.insertText(copiedText);
         return editor.getText();
-      })
-    );
-
-    waitsForPromise(() =>
-      Promise.all([
-        activationPromise,
-        expectedPromise,
-        actualPromise
-      ]).then(values => {
-        const expected = values[1];
-        const actual = values[2];
-        expect(actual).toBe(expected);
-      }
-    ));
-  });
-
-  it('can be toggled.', () => {
-    const expectedPromiseIfOn =
-      helper.handleFileInEditor(
-        pathExpected,
-        editor => editor.getText()
-      );
-    const expectedPromiseIfOff =
-      helper.handleFileInEditor(
-        pathExpectedIfNoExec,
-        editor => editor.getText()
-      );
-
-    const createActualPromise = () => {
-      atom.commands.dispatch(workspaceElement, 'html-elmfier:toggle');
-      return helper.handleFileInEditor(pathCopyFrom, editor => {
-        editor.setSelectedBufferRange([[11, 4], [28, 10]]);
-        // editor.copySelectedText();
-        return editor.getSelectedText();
-      }).then(copiedText =>
-        helper.handleFileInEditor(pathCopyTo, editor => {
-          editor.setCursorBufferPosition([1, 4]);
-          // editor.pasteText();
-          editor.insertText(copiedText);
-          return editor.getText();
-        })
-      );
+      }));
     };
 
     const actuals = [];
@@ -95,19 +87,17 @@ describe('HtmlElmfier', () => {
       .then(() => createActualPromise())
       .then(value => actuals.push(value));
 
-    waitsForPromise(() =>
-      Promise.all([
-        activationPromise,
-        expectedPromiseIfOn,
-        expectedPromiseIfOff,
-        actualPromise
-      ]).then(values => {
-        const expectedIfOn = values[1];
-        const expectedIfOff = values[2];
-        expect(actuals[0]).toBe(expectedIfOff);
-        expect(actuals[1]).toBe(expectedIfOn);
-        expect(actuals[2]).toBe(expectedIfOff);
-      }
-    ));
+    waitsForPromise(() => Promise.all([
+      activationPromise,
+      expectedPromiseIfOn,
+      expectedPromiseIfOff,
+      actualPromise,
+    ]).then((values) => {
+      const expectedIfOn = values[1];
+      const expectedIfOff = values[2];
+      expect(actuals[0]).toBe(expectedIfOff);
+      expect(actuals[1]).toBe(expectedIfOn);
+      expect(actuals[2]).toBe(expectedIfOff);
+    }));
   });
 });
