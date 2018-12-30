e4d5cd4bb0fba6734d084a7e78d7159bc08920e7
takashi tokumoto <jazzm@ster.jp.net>
2018-12-30 13:52:02 +0900

    インデント修正

--- spec/fixtures/expectedIfNoExec.elm
+++ spec/fixtures/expectedIfNoExec.elm
@@ -1,28 +1,28 @@
 view model =
         <div class="wrapper" id="wrapper">
-            This is the beginning.
-            <nav class="gNav" id="gNav">
-                <ul class="gNav__list">
-                    <li>文字<!--コメント-->文字</li>
-                    <li><div>&nbsp;&nbsp;&lt;&gt;&amp;&nbsp;&nbsp;&quot;&#39;&nbsp;&nbsp;</div></li>
-                    <li><a href="aaa.html"><span>link"li nk'link</span></a></li>
-                    <li><a href="aaa.html"><span>link&quot;li&nbsp;nk&#39;link</span></a></li>
-                    <li><a href="aaa.html"><span>
-                      link
-                      link
-                      link
-                    </span></a></li>
-                    <li><a href="aaa.html"><span>link</span></a></li>
-                </ul>
-            </nav>
-            When <b>you</b> gaze into the <b>abyss</b>,<br>
-            the <b>abyss</b> gazes into <b>you</b>.
-            <table class="table_class">
-                <tr>
-                    <td colspan="3" rowspan="3">td</td>
-                </tr>
-            </table>
-            <input type="button" value="-"/>
-            <div>0</div>
-            <input type="button" value="+"/>
-        </div>
+        This is the beginning.
+        <nav class="gNav" id="gNav">
+            <ul class="gNav__list">
+                <li>文字<!--コメント-->文字</li>
+                <li><div>&nbsp;&nbsp;&lt;&gt;&amp;&nbsp;&nbsp;&quot;&#39;&nbsp;&nbsp;</div></li>
+                <li><a href="aaa.html"><span>link"li nk'link</span></a></li>
+                <li><a href="aaa.html"><span>link&quot;li&nbsp;nk&#39;link</span></a></li>
+                <li><a href="aaa.html"><span>
+                  link
+                  link
+                  link
+                </span></a></li>
+                <li><a href="aaa.html"><span>link</span></a></li>
+            </ul>
+        </nav>
+        When <b>you</b> gaze into the <b>abyss</b>,<br>
+        the <b>abyss</b> gazes into <b>you</b>.
+        <table class="table_class">
+            <tr>
+                <td colspan="3" rowspan="3">td</td>
+            </tr>
+        </table>
+        <input type="button" value="-"/>
+        <div>0</div>
+        <input type="button" value="+"/>
+    </div>
--- test/test.elm
+++ test/test.elm
@@ -1,28 +1,40 @@
 view model =
-        <div class="wrapper" id="wrapper">
-            This is the beginning.
-            <nav class="gNav" id="gNav">
-                <ul class="gNav__list">
-                    <li>文字<!--コメント-->文字</li>
-                    <li><div>&nbsp;&nbsp;&lt;&gt;&amp;&nbsp;&nbsp;&quot;&#39;&nbsp;&nbsp;</div></li>
-                    <li><a href="aaa.html"><span>link"li nk'link</span></a></li>
-                    <li><a href="aaa.html"><span>link&quot;li&nbsp;nk&#39;link</span></a></li>
-                    <li><a href="aaa.html"><span>
-                      link
-                      link
-                      link
-                    </span></a></li>
-                    <li><a href="aaa.html"><span>link</span></a></li>
-                </ul>
-            </nav>
-            When <b>you</b> gaze into the <b>abyss</b>,<br>
-            the <b>abyss</b> gazes into <b>you</b>.
-            <table class="table_class">
-                <tr>
-                    <td colspan="3" rowspan="3">td</td>
-                </tr>
-            </table>
-            <input type="button" value="-"/>
-            <div>0</div>
-            <input type="button" value="+"/>
-        </div>
+        div [ class "wrapper", id "wrapper" ] [
+            text "This is the beginning.",
+            nav [ class "gNav", id "gNav" ] [
+                ul [ class "gNav__list" ] [
+                    li [] [
+                        text "文字",
+                        text "文字" ],
+                    li [] [
+                        div [] [ text "<>& \"\'" ] ],
+                    li [] [
+                        a [ href "aaa.html" ] [
+                            span [] [ text "link\"li nk\'link" ] ] ],
+                    li [] [
+                        a [ href "aaa.html" ] [
+                            span [] [ text "link\"li nk\'link" ] ] ],
+                    li [] [
+                        a [ href "aaa.html" ] [
+                            span [] [ text "link link link" ] ] ],
+                    li [] [
+                        a [ href "aaa.html" ] [
+                            span [] [ text "link" ] ] ] ] ],
+            text "When",
+            b [] [ text "you" ],
+            text "gaze into the",
+            b [] [ text "abyss" ],
+            text ",",
+            br [] [],
+            text "the",
+            b [] [ text "abyss" ],
+            text "gazes into",
+            b [] [ text "you" ],
+            text ".",
+            table [ class "table_class" ] [
+                tbody [] [
+                    tr [] [
+                        td [ colspan 3, rowspan 3 ] [ text "td" ] ] ] ],
+            input [ type_ "button", value "-" ] [],
+            div [] [ text "0" ],
+            input [ type_ "button", value "+" ] [] ]
